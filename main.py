2e57e68aa961685114c49db14e44475b3c39d12f
unknown <kevin.cheung.kai@gmail.com>
2021-03-23 22:28:23 -0400

    implemented countdown functions and timer function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import math
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,11 +11,26 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(1 * 60)
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_second = count % 60
+    if count_second < 10:
+        count_second = f'0{count_second}'
+
+    canvas.itemconfig(timer_text, text=f'{count_min}:{count_second}')
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -23,10 +40,10 @@ window.config(padx=100, pady=50, bg=YELLOW)
 timer_label = Label(text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 36, 'bold'))
 timer_label.grid(column=2, row=0)
 
-check_mark = Label(text='✓', fg=GREEN, font=(FONT_NAME, 12, 'bold'))
+check_mark = Label(text='✓', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 12, 'bold'))
 check_mark.grid(column=2, row=4)
 
-start_button = Button(text='Start')
+start_button = Button(text='Start', command=start_timer)
 start_button.grid(column=1, row=3)
 
 reset_button = Button(text='Reset')
@@ -35,7 +52,7 @@ reset_button.grid(column=3, row=3)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 28, 'bold'))
+timer_text = canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 28, 'bold'))
 canvas.grid(column=2, row=1)
 
 window.mainloop()
