e89a42c0a596fe258810fb3b68856f6a402b126e
unknown <kevin.cheung.kai@gmail.com>
2021-03-23 20:43:21 -0400

    added title, buttons, and checkmark

--- main.py
+++ main.py
@@ -20,10 +20,22 @@ window = Tk()
 window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW)
 
+timer_label = Label(text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 36, 'bold'))
+timer_label.grid(column=2, row=0)
+
+check_mark = Label(text='✓', fg=GREEN, font=(FONT_NAME, 12, 'bold'))
+check_mark.grid(column=2, row=4)
+
+start_button = Button(text='Start')
+start_button.grid(column=1, row=3)
+
+reset_button = Button(text='Reset')
+reset_button.grid(column=3, row=3)
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 28, 'bold'))
-canvas.pack()
+canvas.grid(column=2, row=1)
 
 window.mainloop()
